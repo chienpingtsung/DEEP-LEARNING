0104157ea12aeff29279d91c9b4218a8f93bbbd3
Chienping Tsung <jianpingzong@outlook.com>
2021-03-24 14:18:17 +0800

    Update vision.py

--- transforms/vision.py
+++ transforms/vision.py
@@ -1,3 +1,5 @@
+import random
+
 from PIL import Image
 from torchvision.transforms import functional
 
@@ -11,4 +13,22 @@ class Resize:
         rst = []
         for pic in args:
             rst.append(functional.resize(pic, self.size, self.interpolation))
+        return rst
+
+
+class RandomRotation:
+    def __init__(self, min, max, resample=False, expand=False, center=None, fill=None):
+        self.min = min
+        self.max = max
+        self.resample = resample
+        self.expand = expand
+        self.center = center
+        self.fill = fill
+
+    def __call__(self, *args):
+        angle = random.uniform(self.min, self.max)
+
+        rst = []
+        for pic in args:
+            rst.append(functional.rotate(pic, angle, self.resample, self.expand, self.center, self.fill))
         return rst

b02f36cae40185df91d1bd191c1f8280015681c5
Chienping Tsung <jianpingzong@outlook.com>
2022-01-05 00:56:07 +0800

    Update vision.py

--- lib/augment/vision.py
+++ lib/augment/vision.py
@@ -14,3 +14,30 @@ class RandomCrop:
         top = random.randint(0, h - th if h - th > 0 else 0)
         left = random.randint(0, w - tw if w - tw > 0 else 0)
         return tuple(functional.crop(img, top, left, th, tw) for img in args)
+
+
+class RandomRotation:
+    def __init__(self, degrees):
+        self.degrees = degrees
+
+    def __call__(self, *args):
+        angle = random.choice(self.degrees)
+        return tuple(functional.rotate(img, angle, expand=True) for img in args)
+
+
+class RandomHorizontalFlip:
+    def __init__(self, p=0.5):
+        self.p = p
+
+    def __call__(self, *args):
+        if random.random() < self.p:
+            return tuple(functional.hflip(img) for img in args)
+
+
+class RandomVerticalFlip:
+    def __init__(self, p=0.5):
+        self.p = p
+
+    def __call__(self, *args):
+        if random.random() < self.p:
+            return tuple(functional.vflip(img) for img in args)
